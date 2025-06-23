af5b8774dfc31bbbf148467977c0a6a7cab1fefc
juliankoh6 <138428500+juliankoh6@users.noreply.github.com>
2025-06-23 15:39:26 +0800

    Update destinations.js

--- backend/routes/destinations.js
+++ backend/routes/destinations.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/auth');
 const { getLearnInfo } = require('../controllers/destinationController');
+const { updateNotes } = require('../controllers/destinationController');
 const {
   fetchPopularDestinations,
   getSaved,
   saveDestination,
   updateDestination,
-  deleteDestination
+  deleteDestination,
 } = require('../controllers/destinationController');
 
 router.get('/popular/:city', fetchPopularDestinations);
 
-// Apply middleware to protected routes
 router.get('/saved', verifyToken, getSaved);
 router.post('/saved', verifyToken, saveDestination);
 router.put('/saved/:id', verifyToken, updateDestination);
 router.delete('/saved/:id', verifyToken, deleteDestination);
+router.patch('/saved/:id/notes', verifyToken, updateNotes);
 router.get('/learn', getLearnInfo);
 module.exports = router;
