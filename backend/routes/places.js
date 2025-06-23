1ff6650b69b2b41b802d4d5fcfce0a08fdd4b55d
juliankoh6 <138428500+juliankoh6@users.noreply.github.com>
2025-06-23 19:41:46 +0800

    Delete backend/routes/places.js

--- backend/routes/places.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder GET route
-router.get('/', (req, res) => {
-  res.json({ message: 'Places route placeholder' });
-});
-
-module.exports = router;
