2b7b1eb319d8b305e5887698a97d7ea07ab51046
juliankoh6 <138428500+juliankoh6@users.noreply.github.com>
2025-06-25 13:00:24 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express');More actions
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
